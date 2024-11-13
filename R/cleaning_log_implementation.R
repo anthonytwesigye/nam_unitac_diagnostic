752b8fbbbd6fcdd07b20776eea8263881032349b
Twesigye Anthony <anthony.twesigye@un.org>
2024-11-13 22:19:45 +0300

    formatted output data

--- R/cleaning_log_implementation.R
+++ R/cleaning_log_implementation.R
@@ -75,15 +75,22 @@ df_cl_review <- cleaningtools::review_cleaning_log(
   cleaning_log_uuid_column = "uuid",
   cleaning_log_new_value_column = "new_value")
 
+# interview site data
+
+df_interview_site_data <- df_data_with_added_cols %>% 
+  select(-any_of(cols_to_remove)) %>% 
+  filter(location_type %in% c("interview_site"))
+
 # filter log for cleaning
 df_final_cleaning_log <- df_filled_cl_main %>% 
   filter(!question %in% c("duration_audit_sum_all_ms", "duration_audit_sum_all_minutes", "phone_consent"), 
          !uuid %in% c("all")) %>% 
-  filter(!str_detect(string = question, pattern = "\\w+\\/$"))
+  filter(!str_detect(string = question, pattern = "\\w+\\/$")) %>% 
+  filter(uuid %in% df_interview_site_data$`_uuid`)
 
 # create the clean data from the raw data and cleaning log
 df_cleaning_step <- cleaningtools::create_clean_data(
-  raw_dataset = df_data_with_added_cols %>% select(-any_of(cols_to_remove)),
+  raw_dataset = df_interview_site_data,
   raw_data_uuid_column = "_uuid",
   cleaning_log = df_final_cleaning_log,
   cleaning_log_change_type_column = "change_type",
@@ -203,27 +210,141 @@ df_updating_sm_parents_roster <- cts_update_sm_parent_cols(input_df_cleaning_ste
 
 # export datasets ---------------------------------------------------------
 
-list_of_datasets <- list("raw_data" = df_tool_data %>% select(-any_of(cols_to_remove)),
-                         "raw_roster" = df_loop_r_roster,
-                         "cleaned_data" = df_updating_sm_parents$updated_sm_parents %>% 
-                           filter(!`_uuid` %in% df_remove_survey_cl$uuid),
-                         "cleaned_roster" = df_updating_sm_parents_roster$updated_sm_parents %>% 
-                           filter(!`_submission__uuid` %in% df_remove_survey_cl$uuid, location_type %in% c("interview_site")) %>% 
-                           select(-cleaning_uuid),
-                         "poi_data" = df_updating_sm_parents$updated_sm_parents %>% 
-                           filter(location_type %in% c("poi")),
+df_out_raw_data <- df_tool_data %>% select(-any_of(cols_to_remove))
+df_out_clean_data <- df_updating_sm_parents$updated_sm_parents %>% 
+  filter(!`_uuid` %in% df_remove_survey_cl$uuid)
+df_out_clean_roster <- df_updating_sm_parents_roster$updated_sm_parents %>% 
+  filter(!`_submission__uuid` %in% df_remove_survey_cl$uuid) %>% 
+  select(-cleaning_uuid)
+
+
+# list_of_datasets <- list("raw_data" = df_out_raw_data,
+#                          "raw_roster" = df_loop_r_roster,
+#                          "cleaned_data" = df_out_clean_data,
+#                          "cleaned_roster" = df_out_clean_roster,
+#                          "cleaned_poi" = df_cleaning_step_poi
+# )
+# 
+# openxlsx::write.xlsx(list_of_datasets,
+#                      paste0("outputs/", butteR::date_file_prefix(), "_diagnostic_of_informality_data.xlsx"),
+#                      overwrite = TRUE)
+# 
+# # export datasets ---------------------------------------------------------
+# 
+# list_of_extra_logs <- list("extra_log_sm_parents" = df_updating_sm_parents$extra_log_sm_parents,
+#                            "extra_log_sm_parents_roster" = df_updating_sm_parents_roster$extra_log_sm_parents
+# )
+# 
+# openxlsx::write.xlsx(list_of_extra_logs,
+#                      paste0("outputs/", butteR::date_file_prefix(), "_extra_sm_parent_changes_checks_diagnostic_of_informality.xlsx"),
+#                      overwrite = TRUE)
+
+
+# format export -----------------------------------------------------------
+
+df_added_categories <- tibble::tribble(
+  ~parent_qn,                                                                    ~question_label,                           ~new_category,
+  "barriers_adopting_digital_tools", "TE1.9.2 What barriers do you face in adopting digital tools for your enterprise?",                        "6 - No barrier",
+  "wc_work_location",                                                     "WC1.12 Do you mainly work in",                  "7 - Home of employer",
+  "reasons_not_using_bank",                                     "WC1.7d Why are you not using a bank account?",                "8 - Employer pays cash",
+  "ow_exposure_mitigation",                   "OW1.12b Have you done the following to mitigate the exposures:",          "4 - Reporting to authorities",
+  "hold_following_namibian_ids",                              "OD1.1.2a You hold the following identity documents:",                          "5 - Passport",
+  "hold_following_namibian_ids",                              "OD1.1.2a You hold the following identity documents:",                   "6 - Driving license",
+  "how_far_do_customers_travel",       "OW1.5c How far do most of your customers travel to reach your enterprises?", "4 - Within and outside the settlement",
+  "highest_educ_level",                                          "DP1.4 Highest level at school completed",                          "11 - Diploma",
+  "type_of_work_done",                                                     "DP1.7 Type of main work done",                        "23 - Bartender",
+  "type_of_work_done",                                                     "DP1.7 Type of main work done",                        "25 - Tailoring",
+  "type_of_work_done",                                                     "DP1.7 Type of main work done",                          "26 - Butcher",
+  "type_of_work_done",                                                     "DP1.7 Type of main work done",                   "27 - Security Guard",
+  "type_of_work_done",                                                     "DP1.7 Type of main work done",                          "28 - Fishing"
 )
 
-openxlsx::write.xlsx(list_of_datasets,
-                     paste0("outputs/", butteR::date_file_prefix(), "_diagnostic_of_informality_data.xlsx"),
-                     overwrite = TRUE)
+# datasets to export
+# "raw_data" = df_out_raw_data,
+# "raw_roster" = df_loop_r_roster,
+# "cleaned_data" = df_out_clean_data,
+# "cleaned_roster" = df_out_clean_roster,
+# "cleaned_poi" = df_cleaning_step_poi
 
-# export datasets ---------------------------------------------------------
+wb_cleaned_data <- createWorkbook()
 
-list_of_extra_logs <- list("extra_log_sm_parents" = df_updating_sm_parents$extra_log_sm_parents,
-                           "extra_log_sm_parents_roster" = df_updating_sm_parents_roster$extra_log_sm_parents
-)
+hs1 <- createStyle(fgFill = "#7B7B7B", textDecoration = "Bold", fontName = "Arial Narrow", fontColour = "white", fontSize = 12, wrapText = F)
 
-openxlsx::write.xlsx(list_of_extra_logs,
-                     paste0("outputs/", butteR::date_file_prefix(), "_extra_sm_parent_changes_checks_diagnostic_of_informality.xlsx"),
-                     overwrite = TRUE)
+modifyBaseFont(wb = wb_cleaned_data, fontSize = 11, fontName = "Arial Narrow")
+
+## raw_data
+addWorksheet(wb_cleaned_data, sheetName="raw_data")
+setColWidths(wb = wb_cleaned_data, sheet = "raw_data", cols = 1:ncol(df_out_raw_data), widths = 24.89)
+writeDataTable(wb = wb_cleaned_data, sheet = "raw_data", 
+               x = df_out_raw_data , 
+               startRow = 1, startCol = 1, 
+               tableStyle = "TableStyleLight9",
+               headerStyle = hs1)
+# freeze pane
+freezePane(wb = wb_cleaned_data, "raw_data", firstActiveRow = 2, firstActiveCol = 2)
+
+## raw_roster 
+addWorksheet(wb_cleaned_data, sheetName="raw_roster")
+setColWidths(wb = wb_cleaned_data, sheet = "raw_roster", cols = 1:ncol(df_loop_r_roster), widths = 24.89)
+writeDataTable(wb = wb_cleaned_data, sheet = "raw_roster", 
+               x = df_loop_r_roster , 
+               startRow = 1, startCol = 1, 
+               tableStyle = "TableStyleLight9",
+               headerStyle = hs1)
+# freeze pane
+freezePane(wb = wb_cleaned_data, "raw_roster", firstActiveRow = 2, firstActiveCol = 2)
+
+## cleaned_data 
+addWorksheet(wb_cleaned_data, sheetName="cleaned_data")
+setColWidths(wb = wb_cleaned_data, sheet = "cleaned_data", cols = 1:ncol(df_out_clean_data), widths = 24.89)
+writeDataTable(wb = wb_cleaned_data, sheet = "cleaned_data", 
+               x = df_out_clean_data , 
+               startRow = 1, startCol = 1, 
+               tableStyle = "TableStyleLight9",
+               headerStyle = hs1)
+# freeze pane
+freezePane(wb = wb_cleaned_data, "cleaned_data", firstActiveRow = 2, firstActiveCol = 2)
+
+## cleaned_roster 
+addWorksheet(wb_cleaned_data, sheetName="cleaned_roster")
+setColWidths(wb = wb_cleaned_data, sheet = "cleaned_roster", cols = 1:ncol(df_out_clean_roster), widths = 24.89)
+writeDataTable(wb = wb_cleaned_data, sheet = "cleaned_roster", 
+               x = df_out_clean_roster , 
+               startRow = 1, startCol = 1, 
+               tableStyle = "TableStyleLight9",
+               headerStyle = hs1)
+# freeze pane
+freezePane(wb = wb_cleaned_data, "cleaned_roster", firstActiveRow = 2, firstActiveCol = 2)
+
+## cleaned_poi 
+addWorksheet(wb_cleaned_data, sheetName="cleaned_poi")
+setColWidths(wb = wb_cleaned_data, sheet = "cleaned_poi", cols = 1:ncol(df_cleaning_step_poi), widths = 24.89)
+writeDataTable(wb = wb_cleaned_data, sheet = "cleaned_poi", 
+               x = df_cleaning_step_poi , 
+               startRow = 1, startCol = 1, 
+               tableStyle = "TableStyleLight9",
+               headerStyle = hs1)
+# freeze pane
+freezePane(wb = wb_cleaned_data, "cleaned_poi", firstActiveRow = 2, firstActiveCol = 2)
+
+
+## newcategories
+addWorksheet(wb_cleaned_data, sheetName="newcategories")
+setColWidths(wb = wb_cleaned_data, sheet = "newcategories", cols = 1:ncol(df_added_categories), widths = 24.89)
+writeDataTable(wb = wb_cleaned_data, sheet = "newcategories", 
+               x = df_added_categories , 
+               startRow = 1, startCol = 1, 
+               tableStyle = "TableStyleLight9",
+               headerStyle = hs1)
+# freeze pane
+freezePane(wb = wb_cleaned_data, "newcategories", firstActiveRow = 2, firstActiveCol = 2)
+
+# active sheet
+
+activeSheet(wb = wb_cleaned_data) <- "cleaned_data"
+
+
+saveWorkbook(wb_cleaned_data, paste0("outputs/", butteR::date_file_prefix(),"_diagnostic_of_informality_data.xlsx"), overwrite = TRUE)
+openXL(file = paste0("outputs/", butteR::date_file_prefix(),"_diagnostic_of_informality_data.xlsx"))
+
+saveWorkbook(wb_log, paste0("support_files/diagnostic_of_informality_data.xlsx"), overwrite = TRUE)
