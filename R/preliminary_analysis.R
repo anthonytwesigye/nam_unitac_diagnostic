41351d1e061e257492606919f8663af6c15acfba
Twesigye Anthony <anthony.twesigye@un.org>
2024-11-15 16:37:42 +0300

    n_max for guess

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -7,13 +7,13 @@ library(analysistools)
 clean_loc_data <- "inputs/diagnostic_of_informality_cleaned_data.xlsx"
 
 # main data
-clean_data_nms <- names(readxl::read_excel(path = clean_loc_data, n_max = 300, sheet = "cleaned_data"))
+clean_data_nms <- names(readxl::read_excel(path = clean_loc_data, n_max = 1000, sheet = "cleaned_data"))
 clean_c_types <- ifelse(str_detect(string = clean_data_nms, pattern = "_other$"), "text", "guess")
 df_main_clean_data <- readxl::read_excel(clean_loc_data, col_types = clean_c_types, sheet = "cleaned_data")
 
 # loops
 # roster
-clean_data_nms_r_roster <- names(readxl::read_excel(path = clean_loc_data, n_max = 300, sheet = "cleaned_roster"))
+clean_data_nms_r_roster <- names(readxl::read_excel(path = clean_loc_data, n_max = 3000, sheet = "cleaned_roster"))
 clean_c_types_r_roster <- ifelse(str_detect(string = clean_data_nms_r_roster, pattern = "_other$"), "text", "guess")
 df_clean_loop_r_roster <- readxl::read_excel(clean_loc_data, col_types = clean_c_types_r_roster, sheet = "cleaned_roster")
 
