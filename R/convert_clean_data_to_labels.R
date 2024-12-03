39598391cecbf5f1b5b71ff231dfa1fa3ab09c85
Twesigye Anthony <anthony.twesigye@un.org>
2024-12-03 21:38:14 +0300

    looping and updating sm data

--- R/convert_clean_data_to_labels.R
+++ R/convert_clean_data_to_labels.R
@@ -32,7 +32,7 @@ df_tool_select_type <- df_survey %>%
 df_choices_support <- df_choices %>%
   left_join(df_tool_select_type) %>%
   unite("survey_choice_id", qn_name, choice_name, sep = "_", remove = FALSE) %>%
-  select(survey_choice_id, choice_label)
+  select(list_name, survey_choice_id, choice_name, choice_label)
 
 # handle select one -------------------------------------------------------
 
@@ -44,7 +44,50 @@ select_one_cols <- df_tool_select_type %>%
   filter(select_type %in% c("select_one"), qn_name %in% valid_data_cols)
 
 df_main_clean_data_with_so_labels <- df_main_clean_data %>%
-  # mutate(across(.cols = any_of(select_one_cols$qn_name), .fns = ~paste0(cur_column(), "_", .))) %>%
   mutate(across(.cols = any_of(select_one_cols$qn_name), .fns = ~ ifelse(!(is.na(.x)| .x %in% c("NA", "")), paste0(cur_column(), "_", .),.x))) %>%
   mutate(across(.cols = any_of(select_one_cols$qn_name), .fns = ~recode(.x, !!!setNames(df_choices_support$choice_label, df_choices_support$survey_choice_id))))
 
+
+# select multiple cols ----------------------------------------------------
+
+select_multiple_cols <- df_tool_select_type %>%
+  filter(select_type %in% c("select_multiple"), qn_name %in% valid_data_cols)
+
+# loop through the SM questions and data to update the SM cols
+# for a given sm question,
+# - create a named vector,
+# - mutate and replace using the named vector
+# - put escape characters while creating the named vector [\\b]
+
+sm_extract <- select_multiple_cols %>% 
+  pull(qn_name)
+
+df_data_for_update <- df_main_clean_data_with_so_labels
+
+for (sm_col in sm_extract) {
+  current_list_name <- select_multiple_cols %>% 
+    filter(qn_name %in% sm_col) %>% 
+    pull(list_name)
+  current_choices <- df_choices_support %>% 
+    filter(list_name %in% current_list_name) %>% 
+    mutate(choice_name = paste0("\\b", choice_name,"\\b")) %>% 
+    select(choice_name, choice_label)
+  current_choices_vec <- current_choices %>% 
+    pull(choice_label) %>% 
+    set_names(current_choices$choice_name)
+  # print(current_choices_vec)
+  
+  df_current_update <- df_data_for_update %>%
+    mutate(!!sm_col := str_replace_all(string = !!sym(sm_col),  current_choices_vec))
+  
+  df_data_for_update <- df_current_update
+  
+}
+
+
+# check
+# df_main_clean_data_with_so_labels %>% 
+#   select(any_of(sm_extract)) 
+# 
+# df_labels <- df_data_for_update %>% 
+#   select(any_of(sm_extract))
